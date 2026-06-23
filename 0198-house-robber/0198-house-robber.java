897758bf917a8e6a8b9419b39123da5b1da1b7db
Preeti Dey <141465009+Preeti227@users.noreply.github.com>
2026-06-23 21:20:36 +0530

    Time: 0 ms (100%), Space: 42.9 MB (36.65%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,17 @@
+class Solution {
+    int[] dp;
+    public int rob(int[] nums) {
+        dp=new int[nums.length];
+        Arrays.fill(dp,-1);
+        return loot(0,nums);
+    }
+    int loot(int i,int[] nums){
+        if(i>=nums.length) return 0;
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        int pick=nums[i]+loot(i+2,nums);
+        int skip=loot(i+1,nums);
+        return dp[i]=Math.max(pick,skip);
+    }
+}
