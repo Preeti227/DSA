c008d5f377c5987ef67e8abbc86e9cc531d6cd7a
Preeti Dey <141465009+Preeti227@users.noreply.github.com>
2026-07-26 15:24:24 +0530

    Time: 1 ms (61.05%), Space: 46.7 MB (98.58%) - LeetHub

---
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -0,0 +1,31 @@
+class Solution {
+    boolean ans = true;
+    private void bfs(int i,int[][]adj,int[]visited){
+        Queue<Integer>q=new LinkedList<>();
+        q.add(i);
+        visited[i]=0;
+        while(q.size()>0){
+            int front=q.remove();
+            int color=visited[front];
+            for(int ele:adj[front]){
+                if(visited[ele]==visited[front]){//the connected node has the same color
+                ans=false;
+                return;
+                }
+                if(visited[ele]==-1){
+                    visited[ele]=1-color;
+                    q.add(ele);
+                }
+            }
+        }
+    }
+    public boolean isBipartite(int[][] adj) {
+        int n=adj.length;
+        int[] visited=new int[n];
+        Arrays.fill(visited,-1);
+        for(int i=0;i<n;i++){
+            if(visited[i]==-1) bfs(i,adj,visited);
+        }
+        return ans;
+    }
+}
