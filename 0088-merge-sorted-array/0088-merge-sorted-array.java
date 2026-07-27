1260b86416c6294bcaec569fd5aae8112547f2a6
Preeti Dey <141465009+Preeti227@users.noreply.github.com>
2026-07-27 16:06:52 +0530

    Time: 4 ms (17.51%), Space: 44 MB (29.72%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -4,21 +4,20 @@ class Solution {
         int right=0;
         while(left>=0 && right<n){
             if(nums1[left]>nums2[right]){
-                swap(nums1,nums2,left,right);
+                swap(left,right,nums1,nums2);
                 left--;
                 right++;
             }
             else break;
-            
         }
         Arrays.sort(nums1,0,m);
         Arrays.sort(nums2);
-        for(int i = 0; i < n; i++) {
-            nums1[m + i] = nums2[i];
+        for(int i=0;i<n;i++){
+            nums1[m+i]=nums2[i];
         }
         return;
     }
-        void swap(int[] nums1,int[] nums2,int a,int b){
+    void swap(int a,int b,int[] nums1,int[] nums2){
             int temp;
             temp=nums1[a];
             nums1[a]=nums2[b];
