5b2958f00fcce3cca7778c0314f15d9813013008
Preeti Dey <141465009+Preeti227@users.noreply.github.com>
2026-08-19 21:07:23 +0530

    Time: 1222 ms (11.45%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,7 +1,6 @@
-SELECT 
-    d.name AS Department,
-    e.name AS Employee,
-    e.salary AS Salary
+SELECT d.name AS Department,
+       e.name AS Employee,
+       e.salary AS Salary
 FROM Employee e
 JOIN Department d
     ON e.departmentId = d.id
