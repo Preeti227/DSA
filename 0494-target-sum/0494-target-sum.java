2d237b3135755ee85c083259333ca666a025d7f9
Preeti Dey <141465009+Preeti227@users.noreply.github.com>
2026-09-24 20:40:09 +0530

    Time: 8 ms (60.95%), Space: 46.7 MB (28.36%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,14 +1,26 @@
 class Solution {
+    static int sumAbs;
     public int findTargetSumWays(int[] nums, int target) {
-        return helper(0,target,nums);
+        sumAbs=0;
+        for(int ele:nums) sumAbs+=Math.abs(ele);
+        int[][] dp=new int[nums.length][2*sumAbs+1];
+         for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+        return helper(0,0,target,nums,dp);
     }
-    int helper(int i, int target, int[]nums){
+    int helper(int i, int sum,int target, int[]nums, int[][] dp){
         if(i==nums.length){
-            if(target==0) return 1;
+            if(sum==target) return 1;
             else return 0;
         }
-        int add=helper(i+1,target-nums[i],nums);
-        int sub=helper(i+1,target+nums[i],nums);
-        return add+sub;
+        int index = sum + sumAbs;
+        if (dp[i][index] != -1) return dp[i][index];
+
+        int add = helper(i + 1,sum + nums[i],target,nums,dp);
+
+        int subtract = helper(i + 1,sum - nums[i],target,nums,dp);
+
+        return dp[i][index] = add + subtract;
     }
 }
